19c92912e48d1d9dd53b5078b6f8fab0700cf8cf
Nikita <druzhbinn@gmail.com>
2024-07-19 17:38:23 +0300

    STYLE: add comments to Health Text Setter

--- test-1c/Assets/test-1c/Scripts/UI/HealthTextSetter.cs
+++ test-1c/Assets/test-1c/Scripts/UI/HealthTextSetter.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 
 public class HealthTextSetter : MonoBehaviour
 {
+    /// <summary>
+    /// Text to write before health points
+    /// </summary>
     [SerializeField] private string healthText;
+
+    /// <summary>
+    /// TMP Text component of health panel
+    /// </summary>
     [SerializeField] private TextMeshProUGUI healthTMP;
+
+    #region MonoBehavior Methods
 
     private void OnEnable()
     {
@@ -16,8 +25,18 @@ public class HealthTextSetter : MonoBehaviour
         CharacterHealth.OnHealthChanged -= SetHealthText;
     }
 
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Sets new health text
+    /// </summary>
+    /// <param name="currentHealth">Health amount to write</param>
     private void SetHealthText(int currentHealth)
     {
         healthTMP.text = $"{healthText} {currentHealth}";
     }
+
+    #endregion
 }
