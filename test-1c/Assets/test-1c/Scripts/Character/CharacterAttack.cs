02764458d60df42064fdbfe4d80df90c6ec7bb02
Nikita <druzhbinn@gmail.com>
2024-07-19 15:15:25 +0300

    REFACTOR: var name changing and add comments in CharacterAttack

--- test-1c/Assets/test-1c/Scripts/Character/CharacterAttack.cs
+++ test-1c/Assets/test-1c/Scripts/Character/CharacterAttack.cs
@@ -11,12 +11,12 @@ public class CharacterAttack : MonoBehaviour
     /// Fire rate in bullets per second
     /// </summary>
     [SerializeField] private float fireRate = 1f;
-    
+
     /// <summary>
     /// Radius of the character's attack in units
     /// </summary>
     [SerializeField] private float attackRadius = 5f;
-    
+
     /// <summary>
     /// GO prefab of the projectile
     /// </summary>
@@ -26,17 +26,19 @@ public class CharacterAttack : MonoBehaviour
     /// Min time of possible shot
     /// </summary>
     private float _nextFireTime;
-    
+
     /// <summary>
     /// Target's transform to shoot
     /// </summary>
     private Transform _target;
 
+    #region MonoBehavior Methods
+
     private void Update()
     {
         // TODO: Find target with coroutine with cooldown for optimization
         FindTarget();
-        
+
         if (_target)
         {
             RotateTowardsTarget();
@@ -44,45 +46,65 @@ public class CharacterAttack : MonoBehaviour
         }
     }
 
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Finds the target (the nearest enemy) within the attack radius of the character
+    /// </summary>
     private void FindTarget()
     {
         Collider2D[] possibleTargets = Physics2D.OverlapCircleAll(transform.position, attackRadius);
-        
+
         float closestDistance = Mathf.Infinity;
         Transform closestEnemy = null;
 
         foreach (Collider2D possibleTarget in possibleTargets)
-        {
-            if (possibleTarget.TryGetComponent(out IDamageable damageable))
+            if (possibleTarget.TryGetComponent(out IDamageable _))
             {
                 float distance = Vector3.Distance(transform.position, possibleTarget.transform.position);
-                
+
                 if (distance < closestDistance)
                 {
                     closestDistance = distance;
                     closestEnemy = possibleTarget.transform;
                 }
             }
-        }
 
         _target = closestEnemy;
     }
 
+    /// <summary>
+    /// Attack behavior
+    /// </summary>
     private void Attack()
     {
-        if (_target  && Time.time >= _nextFireTime)
+        if (_target && Time.time >= _nextFireTime)
         {
             _nextFireTime = Time.time + 1f / fireRate;
             Shoot();
         }
     }
 
+    /// <summary>
+    /// Spawn projectile
+    /// </summary>
     private void Shoot()
     {
         // TODO: Add object pool
-        GameObject bullet = Instantiate(projectilePrefab, projectileSpawner.position, projectileSpawner.rotation);
+        GameObject projectile = Instantiate(projectilePrefab, projectileSpawner.position, projectileSpawner.rotation);
     }
 
+    /// <summary>
+    /// Rotates character towards target
+    /// </summary>
     private void RotateTowardsTarget()
     {
         Vector3 direction = (_target.position - transform.position).normalized;
@@ -90,9 +112,5 @@ public class CharacterAttack : MonoBehaviour
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
     }
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, attackRadius);
-    }
+    #endregion
 }
