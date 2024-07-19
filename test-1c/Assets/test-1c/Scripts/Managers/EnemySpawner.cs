d613bd398eb5c63964c6f86a53c193fbcb875699
Nikita <druzhbinn@gmail.com>
2024-07-19 17:27:49 +0300

    STYLE: add comments to EnemySpawner

--- test-1c/Assets/test-1c/Scripts/Managers/EnemySpawner.cs
+++ test-1c/Assets/test-1c/Scripts/Managers/EnemySpawner.cs
@@ -3,19 +3,45 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    /// <summary>
+    /// Factory component
+    /// </summary>
     [SerializeField] private EnemyFactory enemyFactory;
+
+    /// <summary>
+    /// Array of spawn points
+    /// </summary>
     [SerializeField] private Transform[] spawnPoints;
 
+    /// <summary>
+    /// Min delay between spawns
+    /// </summary>
     [SerializeField] private float minSpawnDelay;
+
+    /// <summary>
+    /// Max delay between spawns
+    /// </summary>
     [SerializeField] private float maxSpawnDelay;
 
+    /// <summary>
+    /// Bool to control spawn
+    /// </summary>
     private bool _isSpawning = true;
+
+    #region MonoBehavior Methods
 
     private void Start()
     {
         StartCoroutine(SpawnEnemies());
     }
 
+    #endregion
+
+    #region PrivateMethods
+
+    /// <summary>
+    /// Spawns 1 enemy at random spawn point position
+    /// </summary>
     private void SpawnEnemy()
     {
         GameObject currentEnemy = enemyFactory.GetProduct().gameObject;
@@ -24,6 +50,9 @@ public class EnemySpawner : MonoBehaviour
         currentEnemy.transform.position = spawnPointPosition;
     }
 
+    /// <summary>
+    /// Spawns enemies with cooldown
+    /// </summary>
     private IEnumerator SpawnEnemies()
     {
         while (_isSpawning)
@@ -33,8 +62,14 @@ public class EnemySpawner : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Calculates new cooldown time (random between min and max delay)
+    /// </summary>
+    /// <returns>New cooldown time</returns>
     private float GetCooldownTime()
     {
         return Random.Range(minSpawnDelay, maxSpawnDelay);
     }
+
+    #endregion
 }
